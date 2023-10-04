a7412fb00574ac956d73964798a6a3d8bafd7591
Amit <amitkumar03022004@gmail.com>
2023-10-05 01:00:12 +0530

    1:15:00

---
+++ reactthapaapp/src/component/Basics/MenuCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const MenuCard = () => {
+   // css example
+    const myStyle = {
+        color: "blue"
+    };
+  return (
+    <div className="card-container">
+    <div className="card">
+        <div className="card-body">
+            <span className='card-number card-circle subtle'>1</span>
+            <span className='card-author subtle' style={myStyle} >Breakfast</span>
+            <h2 className='card-title'>Maggi</h2>
+            <span className='card-description subtle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, accusantium. Consequuntur doloribus dolor facere earum corporis neque rem beatae culpa!</span>
+            <div className='card-read'>Read</div>
+            {/* <img src="" className='card-media' alt="" /> */}
+            <span className='card-tag subtle'>Order Now</span>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default MenuCard
--- reactthapaapp/src/component/Basics/Restaurant.js
+++ reactthapaapp/src/component/Basics/Restaurant.js
@@ -1,28 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./style.css";
-import Menu from "./menuApi";
-
+import Menu from "./menuApi.js";
+import MenuCard from "./MenuCard";
 
 const Restaurant = () => {
-    // css 
-    const myStyle = {
-        color: "blue"
-    };
+    //use state is here for store the data into react which are come from menuapi.js file . -kr.amitsingh
+    const [menuData,setMenuData]=useState(Menu);
+
+
+
+   
     return (
         <>
-            <div className="card-container">
-                <div className="card">
-                    <div className="card-body">
-                        <span className='card-number card-circle subtle'>1</span>
-                        <span className='card-author subtle' style={myStyle} >Breakfast</span>
-                        <h2 className='card-title'>Maggi</h2>
-                        <span className='card-description subtle'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, accusantium. Consequuntur doloribus dolor facere earum corporis neque rem beatae culpa!</span>
-                        <div className='card-read'>Read</div>
-                        {/* <img src="" className='card-media' alt="" /> */}
-                        <span className='card-tag subtle'>Order Now</span>
-                    </div>
-                </div>
-            </div>
+           <MenuCard menuData = {menuData}></MenuCard>
         </>
     )
 }
